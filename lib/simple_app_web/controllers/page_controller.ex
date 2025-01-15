b623c30d65145784417689c950fc1d98d3238087
Victor Björklund <victor.bjorklund@gmail.com>
2025-01-15 22:14:11 +0100

    fixes

--- lib/simple_app_web/controllers/page_controller.ex
+++ lib/simple_app_web/controllers/page_controller.ex
@@ -6,4 +6,11 @@ defmodule SimpleAppWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+
+
+  def about(conn, _params) do
+    # The about page is often custom made,
+    # so skip the default app layout.
+    render(conn, :about, layout: false)
+  end
 end
--- lib/simple_app_web/router.ex
+++ lib/simple_app_web/router.ex
@@ -18,6 +18,7 @@ defmodule SimpleAppWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    get "/about", PageController, :about
   end
 
   # Other scopes may use custom stacks.
